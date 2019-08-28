20267708b4e243384a7818542a645ae6693d38fb
Sergio Colmenares <azuresynchro@gmail.com>
2019-08-28 18:35:12 -0500

    I add the routing functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { GitSearchService } from './git-search.service'
 import { GitSearchUsersService } from './git-search-users.service';
 import { GitSearchComponent } from './git-search/git-search.component';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'search',
+    component: GitSearchComponent,
+    data: {
+      title: 'Git Search Title'
+    }
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitSearchComponent
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule  
+    FormsModule,
+    RouterModule.forRoot(appRoutes)  
   ],
   providers: [GitSearchService, GitSearchUsersService],
   bootstrap: [AppComponent]
